d1a6aa0345b7a7b209894ef5d3732eee17b9d217
SergeiIonin <sai1042@yandex.ru>
2024-08-10 23:50:10 -0400

    fixes for schema-registry response decoding

--- rest-api/src/main/scala/serverendpoints/ContractsServerEndpoints.scala
+++ rest-api/src/main/scala/serverendpoints/ContractsServerEndpoints.scala
@@ -14,20 +14,24 @@ import io.github.sergeiionin.contractsregistrator.repository.ContractsRepository
 import sttp.tapir.server.ServerEndpoint.Full
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
 import org.http4s.Uri
+import io.github.sergeiionin.contractsregistrator.dto.schemaregistry.DTO.*
 
 class ContractsServerEndpoints[F[_] : Async : MonadThrow](baseUri: String, client: ContractsRegistryHttpClient[F]) extends ContractsEndpoints:
-  given createContractDTOEncoder: EntityEncoder[F, ContractDTO] = jsonEncoderOf[F, ContractDTO]
-  given createContractDTODecoder: EntityDecoder[F, ContractDTO] = jsonOf[F, ContractDTO]
+  given createContractDtoEncoder: EntityEncoder[F, ContractDTO] = jsonEncoderOf[F, ContractDTO]
+  given createContractDtoDecoder: EntityDecoder[F, ContractDTO] = jsonOf[F, ContractDTO]
   
-  given createContractResponseDTOEncoder: EntityEncoder[F, CreateContractResponseDTO] = jsonEncoderOf[F, CreateContractResponseDTO]
-  given createContractResponseDTODecoder: EntityDecoder[F, CreateContractResponseDTO] = jsonOf[F, CreateContractResponseDTO]
+  given createContractResponseDtoEncoder: EntityEncoder[F, CreateContractResponseDTO] = jsonEncoderOf[F, CreateContractResponseDTO]
+  given createContractResponseDtoDecoder: EntityDecoder[F, CreateContractResponseDTO] = jsonOf[F, CreateContractResponseDTO]
+  
+  given createSchemaResponseDtoEncoder: EntityEncoder[F, CreateSchemaResponseDTO] = jsonEncoderOf[F, CreateSchemaResponseDTO]
+  given createSchemaResponseDtoDecoder: EntityDecoder[F, CreateSchemaResponseDTO] = jsonOf[F, CreateSchemaResponseDTO]
 
   val createContractSE: ServerEndpoint[Any, F] =
     createContract.serverLogic(createContract => {
       val contract = ContractDTO(schema = createContract.schema)
       client.post(Uri.unsafeFromString(s"$baseUri/subjects/${createContract.name}/versions"), contract, None).flatMap {
         case response if response.status.code == 200 =>
-          response.as[CreateContractResponseDTO].map(_.asRight[ContractErrorDTO])
+          response.as[CreateSchemaResponseDTO].map(r => CreateContractResponseDTO(createContract.name, r.id).asRight[ContractErrorDTO])
         case response if response.status.code >= 400 && response.status.code < 500 =>
           BadRequestDTO(createContract.name, "FIXME").asLeft[CreateContractResponseDTO].pure[F]
       }
