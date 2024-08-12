eaa423e6cb5596783cd371620afb8e16a5e604a7
SergeiIonin <sai1042@yandex.ru>
2024-08-12 19:57:22 -0400

    add delete functionality to ContractsHandler

--- reader/src/main/scala/handler/ContractsHandler.scala
+++ reader/src/main/scala/handler/ContractsHandler.scala
@@ -5,14 +5,16 @@ import cats.effect.Resource
 import cats.Monad
 import domain.Contract
 import repository.ContractsRepository
-
 import github.GitClient
 
+import org.typelevel.log4cats.Logger
+
 trait ContractsHandler[F[_]]:
-  def handle(contract: Contract): F[Unit]
+  def addContract(contract: Contract): F[Unit]
+  def deleteContract(subject: String, version: Int): F[Unit]
 
 object ContractsHandler:
   def make[F[_] : Monad](repository: ContractsRepository[F],
                           gitClient: GitClient[F]
-                        ): Resource[F, ContractsHandler[F]] =
+                        )(using Logger[F]): Resource[F, ContractsHandler[F]] =
     Resource.pure[F, ContractsHandler[F]](ContractsHandlerImpl[F](repository, gitClient))
