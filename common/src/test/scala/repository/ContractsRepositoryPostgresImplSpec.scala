a2cc3918bcd436b2ec512b57f6ce42eb3d040bc3
SergeiIonin <sai1042@yandex.ru>
2024-08-15 13:00:25 -0400

    add deleting of multiple contracts for the same subject assertion

--- common/src/test/scala/repository/ContractsRepositoryPostgresImplSpec.scala
+++ common/src/test/scala/repository/ContractsRepositoryPostgresImplSpec.scala
@@ -19,7 +19,10 @@ import org.typelevel.log4cats.{Logger, LoggerFactory}
 import domain.Contract
 
 class ContractsRepositoryPostgresImplSpec extends Specification with CatsEffect:
-
+  import ContractsRepositoryPostgresImplSpec.given
+  
+  val logger: Logger[IO] = summon[Logger[IO]]
+  
   val container: PostgreSQLContainer = PostgreSQLContainer()
   
   def initPostgres(session: Session[IO]): IO[Unit] = 
@@ -39,29 +42,26 @@ class ContractsRepositoryPostgresImplSpec extends Specification with CatsEffect:
   val testSchema2: String = "testSchema_2"
   val testContractV1: Contract = Contract(testSubject, testVersion1, testId1, testSchema1)
   val testContractV2: Contract = Contract(testSubject, testVersion2, testId2, testSchema2)
-  
+
+  val postgresResource: Resource[IO, PostgreSQLContainer] =
+    Resource.make(IO.delay {
+      container.start()
+      container
+    })(c => {
+      IO.delay(c.stop())
+    })
+
+  def sessionResource(container: PostgreSQLContainer): Resource[IO, Session[IO]] =
+    Session.single[IO](
+      host = container.containerIpAddress,
+      port = container.firstMappedPort,
+      user = container.username,
+      password = Some(container.password),
+      database = container.databaseName
+    )
+
   "ContractsRepositoryPostgresImpl" should {
     "perform CRUD operations correctly" in {
-      val postgresResource: Resource[IO, PostgreSQLContainer] =
-        Resource.make(IO.delay {
-          container.start()
-          container
-        })(c => {
-          IO.delay(c.stop())
-        })
-      
-      
-      def sessionResource(container: PostgreSQLContainer): Resource[IO, Session[IO]] =
-        Session.single[IO](
-          host = container.containerIpAddress,
-          port = container.firstMappedPort,
-          user = container.username,
-          password = Some(container.password),
-          database = container.databaseName
-        )
-
-      given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
-
       (for
         postgres <- postgresResource
         session <- sessionResource(postgres)
@@ -78,10 +78,25 @@ class ContractsRepositoryPostgresImplSpec extends Specification with CatsEffect:
           versionsS <- repository.getAllVersionsForSubject(testSubject)
           versions  <- versionsS.compile.toList
           _         =  versions must beEqualTo(List(1, 2))
-          _         <- repository.delete(testSubject, 1)
-          resOpt1   <- repository.get(testSubject, 1)
+          _         <- repository.delete(testSubject, 2)
+          resOpt1   <- repository.get(testSubject, 2)
           _         =  resOpt1 must beNone
+          _         <- repository.save(testContractV2)
+          versionsS <- repository.getAllVersionsForSubject(testSubject)
+          versions  <- versionsS.compile.toLyist
+          _         =  versions must beEqualTo(List(1, 2))
+          _         <- versionsS.parEvalMapUnordered(10)(version =>
+                          repository.delete(testSubject, version)
+                       ).compile.drain
+          resOpt1   <- repository.get(testSubject, 1)
+          resOpt2   <- repository.get(testSubject, 2)
+          _         = resOpt1 must beNone
+          _         = resOpt2 must beNone
         yield true
       }
     }
   }
+  
+object ContractsRepositoryPostgresImplSpec:
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+    
