66974a6efdf039ef602ab93ae33c616f244765fd
SergeiIonin <sai1042@yandex.ru>
2024-08-16 23:45:05 -0400

    add init.sql

---
+++ postgres/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE registry.contracts (
+    subject VARCHAR NOT NULL,
+    version INTEGER NOT NULL,
+    id INTEGER NOT NULL,
+    isMerged BOOLEAN NOT NULL,
+    schema TEXT NOT NULL,
+    PRIMARY KEY (subject, id)
+);
+
