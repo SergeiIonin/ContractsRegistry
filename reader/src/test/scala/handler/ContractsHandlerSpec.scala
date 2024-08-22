81d9f4d0a8ad146df976145e36b3c534320d5a76
SergeiIonin <sai1042@yandex.ru>
2024-08-21 22:03:00 -0400

    [reader] fix ContractsHandlerSpec to properly terminate ryuk containers

--- reader/src/test/scala/handler/ContractsHandlerSpec.scala
+++ reader/src/test/scala/handler/ContractsHandlerSpec.scala
@@ -1,27 +1,29 @@
 package io.github.sergeiionin.contractsregistrator
 package handler
 
-import cats.effect.IO
-import cats.effect.Resource
+import repository.PostgresHelper
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import org.scalatest.wordspec.AnyWordSpec
+import domain.Contract
+import domain.SchemaType.PROTOBUF
+import github.{GitHubClient, GitHubClientTestImpl}
+import repository.{ContractsRepository, PostgresHelper}
+import cats.effect.{IO, Resource}
 import cats.effect.unsafe.implicits.global
-import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
-import cats.effect.testing.specs2.CatsEffect
-import org.specs2.mutable.Specification
-import org.specs2.matcher.ShouldMatchers
-import org.specs2.mutable.*
-import skunk.Session
-import skunk.codec.all.*
-import skunk.*
-import skunk.implicits.*
+import com.dimafeng.testcontainers.{Container, ForAllTestContainer, PostgreSQLContainer}
 import natchez.Trace.Implicits.noop
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.testcontainers.utility.DockerImageName
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.{Logger, LoggerFactory}
-import domain.Contract
-import repository.{ContractsRepository, PostgresHelper}
-import github.{GitHubClient, GitHubClientTestImpl}
-import domain.SchemaType.PROTOBUF
+import skunk.*
+import skunk.codec.all.*
+import skunk.implicits.*
 
-class ContractsHandlerSpec extends Specification with CatsEffect with PostgresHelper[IO]:
+// fixme when this test is run through sbt, a container don't shutdown
+class ContractsHandlerSpec extends AnyWordSpec with Matchers with TestContainerForAll with PostgresHelper[IO]:
   import ContractsHandlerSpec.given
 
   val logger: Logger[IO] = summon[Logger[IO]]
@@ -35,34 +37,49 @@ class ContractsHandlerSpec extends Specification with CatsEffect with PostgresHe
   val testSchema2: String = "testSchema_2"
   val testContractV1: Contract = Contract(testSubject, testVersion1, testId1, testSchema1, PROTOBUF)
   val testContractV2: Contract = Contract(testSubject, testVersion2, testId2, testSchema2, PROTOBUF)
-  
+
+  override val containerDef = PostgreSQLContainer.Def(
+    dockerImageName = DockerImageName.parse("postgres:16.3"),
+    databaseName = "testcontainer-scala",
+    username = "scala",
+    password = "scala"
+  )
+
+  override def beforeContainersStop(containers: Containers): Unit = {
+    super.beforeContainersStop(containers)
+    println("Shutting down ryuk container...")
+    after_All()
+  }
+
   "ContractsHandler" should {
     "add, set isMerged status and delete contracts" in {
-      (for
-        postgres  <- postgresResource
-        session   <- sessionPooledResource(postgres)
-        _         <- Resource.eval(initPostgres(session))
-        repo      <- ContractsRepository.make[IO](session)
-        gitClient <- GitHubClient.test[IO]()
-        handler   <- ContractsHandler.make[IO](repo, gitClient)
-      yield (handler, repo)).use { (h: ContractsHandler[IO], r: ContractsRepository[IO]) => {
+      withContainers { container =>
+        val psqlContainer: PostgreSQLContainer = container.asInstanceOf[PostgreSQLContainer]
+        (for
+          session <- sessionPooledResource(psqlContainer)
+          _ <- Resource.eval(initPostgres(session))
+          repo <- ContractsRepository.make[IO](session)
+          gitClient <- GitHubClient.test[IO]()
+          handler <- ContractsHandler.make[IO](repo, gitClient)
+        yield (handler, repo)).use { (h: ContractsHandler[IO], r: ContractsRepository[IO]) => {
           for
             _ <- h.addContract(testContractV1)
             _ <- h.addContract(testContractV2)
             versionsS <- r.getAllVersionsForSubject(testSubject)
             versions <- versionsS.compile.toList
             _ <- logger.info(s"versions before deleting: $versions")
-            _ = versions must beEqualTo(List(1, 2))
+            _ = versions shouldBe List(1, 2)
             _ <- h.updateIsMergedStatus(testSubject, testVersion2)
             contractOpt <- r.get(testSubject, testVersion2)
             _ <- logger.info(s"contract after setting isMerged: $contractOpt")
-            _ = contractOpt.exists(_.isMerged) must beTrue
+            _ = contractOpt.exists(_.isMerged) shouldBe true
             _ <- h.deleteContract(testSubject)
             versionsS <- r.getAllVersionsForSubject(testSubject)
             versions <- versionsS.compile.toList
-            _ = versions must beEmpty
+            _ = versions shouldBe Nil
           yield true
         }
+        }.unsafeRunSync()
       }
     }
   }
