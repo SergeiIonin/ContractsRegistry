90de47c97c92a3a1f04d318663bbe9c6a14eec77
SergeiIonin <sai1042@yandex.ru>
2024-08-21 22:01:33 -0400

    [common] add after_All to PostgresHelper as a workaround for utilization of ryuk sidecar testcontainers

--- common/src/test/scala/repository/PostgresHelper.scala
+++ common/src/test/scala/repository/PostgresHelper.scala
@@ -1,35 +1,44 @@
 package io.github.sergeiionin.contractsregistrator
 package repository
 
+import scala.util.{Try, Success, Failure}
 import cats.syntax.option.*
 import cats.effect.IO
 import cats.effect.Resource
 import cats.effect.unsafe.implicits.global
-import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import com.dimafeng.testcontainers.{Container, ForAllTestContainer, PostgreSQLContainer}
 import cats.effect.testing.specs2.CatsEffect
 import org.specs2.mutable.Specification
 import org.specs2.matcher.ShouldMatchers
 import org.specs2.mutable.*
 import skunk.Session
-import skunk.codec.all._
+import skunk.codec.all.*
 import skunk.*
 import skunk.implicits.*
 import natchez.Trace.Implicits.noop
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 import domain.Contract
+import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.core.{DefaultDockerClientConfig, DockerClientBuilder}
+
+import org.scalatest.BeforeAndAfterAll
 
 trait PostgresHelper[F[_]]:
-  lazy val container: PostgreSQLContainer = PostgreSQLContainer()
 
-  lazy val postgresResource: Resource[IO, PostgreSQLContainer] =
-    Resource.make(IO.delay {
-      container.start()
-      container
-    })(c => {
-      IO.delay(c.stop())
-    })
-
+  // todo this is a hack for the case when sidecar ryuk-containers weren't terminated after test. Any chance to make it better?
+  def after_All(): Unit =
+    val dockerClient: DockerClient = DockerClientBuilder.getInstance(DefaultDockerClientConfig.createDefaultConfigBuilder().build()).build()
+    dockerClient.listContainersCmd().exec().forEach { container =>
+      if (container.getImage.contains("testcontainers")) {
+        val id = container.getId
+        println(s"removing container: ${container.getNames.toList.mkString(", ")}, id: ${container.getId}")
+        Try(dockerClient.stopContainerCmd(id).exec()) match 
+          case Failure(e) => println(s"error stopping ryuk container: ${e.getMessage}")
+          case Success(_) => ()
+      }
+    }
+  
   def initPostgres(session: Session[IO]): IO[Unit] =
     session.execute(
       sql"""CREATE TABLE contracts(
@@ -63,12 +72,13 @@ trait PostgresHelper[F[_]]:
       database = container.databaseName
     )
 
-  def sessionPooledResource(container: PostgreSQLContainer): Resource[IO, Resource[IO, Session[IO]]] =
+  def sessionPooledResource(container: Container): Resource[IO, Resource[IO, Session[IO]]] =
+    val postgresContainer = container.asInstanceOf[PostgreSQLContainer]
     Session.pooled[IO](
-      host = container.containerIpAddress,
-      port = container.firstMappedPort,
-      user = container.username,
-      password = container.password.some,
-      database = container.databaseName,
+      host = postgresContainer.containerIpAddress,
+      port = postgresContainer.firstMappedPort,
+      user = postgresContainer.username,
+      password = postgresContainer.password.some,
+      database = postgresContainer.databaseName,
       max = 10
     ) 
