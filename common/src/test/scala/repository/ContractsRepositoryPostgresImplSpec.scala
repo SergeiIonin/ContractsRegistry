2939c83555c64db456bec82ad29ed90f8baf96e6
SergeiIonin <sai1042@yandex.ru>
2024-08-21 22:02:25 -0400

    [common] fix ContractsRepositoryPostgresImplSpec to properly terminate ryuk containers

--- common/src/test/scala/repository/ContractsRepositoryPostgresImplSpec.scala
+++ common/src/test/scala/repository/ContractsRepositoryPostgresImplSpec.scala
@@ -1,48 +1,59 @@
 package io.github.sergeiionin.contractsregistrator
 package repository
 
-import cats.syntax.option.*
-import cats.effect.IO
-import cats.effect.Resource
-import cats.effect.unsafe.implicits.global
-import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
-import cats.effect.testing.specs2.CatsEffect
-import org.specs2.mutable.Specification
-import org.specs2.matcher.ShouldMatchers
-import org.specs2.mutable.*
-import skunk.Session
-import skunk.codec.all.*
-import skunk.*
-import skunk.implicits.*
-import natchez.Trace.Implicits.noop
-import org.typelevel.log4cats.slf4j.Slf4jLogger
-import org.typelevel.log4cats.{Logger, LoggerFactory}
 import domain.Contract
-
 import domain.SchemaType.PROTOBUF
 
-class ContractsRepositoryPostgresImplSpec extends Specification with CatsEffect with PostgresHelper[IO]:
+import cats.effect.unsafe.implicits.global
+import cats.effect.{IO, Resource}
+import cats.syntax.option.*
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
+import com.dimafeng.testcontainers.{Container, ForAllTestContainer, ForEachTestContainer, PostgreSQLContainer}
+import natchez.Trace.Implicits.noop
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.testcontainers.utility.DockerImageName
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.{Logger, LoggerFactory}
+import skunk.*
+import skunk.codec.all.*
+import skunk.implicits.*
+
+class ContractsRepositoryPostgresImplSpec extends AnyWordSpec with Matchers with TestContainerForAll with PostgresHelper[IO]:
   import ContractsRepositoryPostgresImplSpec.given
-  
+
   val logger: Logger[IO] = summon[Logger[IO]]
 
   val testSubject: String = "testSubject"
   val testId1: Int = 3
-  val testId2: Int = testId1+1
+  val testId2: Int = testId1 + 1
   val testVersion1: Int = 1
-  val testVersion2: Int = testVersion1+1
+  val testVersion2: Int = testVersion1 + 1
   val testSchema1: String = "testSchema_1"
   val testSchema2: String = "testSchema_2"
   val testContractV1: Contract = Contract(testSubject, testVersion1, testId1, testSchema1, PROTOBUF)
   val testContractV2: Contract = Contract(testSubject, testVersion2, testId2, testSchema2, PROTOBUF)
-  
+
+  override val containerDef = PostgreSQLContainer.Def(
+    dockerImageName = DockerImageName.parse("postgres:16.3"),
+    databaseName = "testcontainer-scala",
+    username = "scala",
+    password = "scala"
+  )
+
+  override def beforeContainersStop(containers: Containers): Unit =
+    super.beforeContainersStop(containers)
+    println("Shutting down ryuk container...")
+    after_All()
+
   "ContractsRepositoryPostgresImpl" should {
     "perform CRUD operations correctly" in {
-      (for
-        postgres <- postgresResource
-        sessionR <- sessionPooledResource(postgres)
-        repo <- ContractsRepository.make[IO](sessionR)
-      yield (sessionR, repo)).use { (sR, repository) => {
+      withContainers { container =>
+        val psqlContainer: PostgreSQLContainer = container.asInstanceOf[PostgreSQLContainer]
+        (for
+          sessionR <- sessionPooledResource(psqlContainer)
+          repo <- ContractsRepository.make[IO](sessionR)
+        yield (sessionR, repo)).use { (sR, repository) => {
           for
             _ <- initPostgres(sR)
             _ <- repository.save(testContractV1)
@@ -50,33 +61,34 @@ class ContractsRepositoryPostgresImplSpec extends Specification with CatsEffect 
             resOpt1 <- repository.get(testSubject, 1)
             _ <- IO.whenA(resOpt1.isEmpty)(IO.raiseError(new RuntimeException("Contract not found")))
             res1 = resOpt1.get
-            _ = res1 must beEqualTo(testContractV1)
+            _ = res1 shouldBe testContractV1
             versionsS <- repository.getAllVersionsForSubject(testSubject)
             versions <- versionsS.compile.toList
-            _ = versions must beEqualTo(List(1, 2))
+            _ = versions shouldBe List(1, 2)
             _ <- repository.delete(testSubject, 2)
             resOpt2 <- repository.get(testSubject, 2)
-            _ = resOpt2 must beNone
+            _ = resOpt2 shouldBe None
             _ <- repository.save(testContractV2)
             _ <- repository.updateIsMerged(testContractV2.subject, testContractV2.version)
             resOpt2 <- repository.get(testSubject, 2)
-            _ = resOpt2.exists(_.isMerged) must beTrue
+            _ = resOpt2.exists(_.isMerged) shouldBe true
             versionsS <- repository.getAllVersionsForSubject(testSubject)
             versions <- versionsS.compile.toList
-            _ = versions must beEqualTo(List(1, 2))
+            _ = versions shouldBe List(1, 2)
             _ <- versionsS.parEvalMapUnordered(10)(version =>
-                    repository.delete(testSubject, version)
-                 ).compile.drain
+              repository.delete(testSubject, version)
+            ).compile.drain
             resOpt1 <- repository.get(testSubject, 1)
             resOpt2 <- repository.get(testSubject, 2)
-            _ = resOpt1 must beNone
-            _ = resOpt2 must beNone
+            _ = resOpt1 shouldBe None
+            _ = resOpt2 shouldBe None
           yield true
         }
+        }.unsafeRunSync()
       }
     }
   }
-  
+
 object ContractsRepositoryPostgresImplSpec:
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
     
