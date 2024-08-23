53d135e9c90de5b5af977a758edf755fe53935aa
SergeiIonin <sai1042@yandex.ru>
2024-08-23 11:25:57 -0400

    [common] fix Decoder for Contract to properly address missing isMerged field in json

--- common/src/main/scala/domain/Contract.scala
+++ common/src/main/scala/domain/Contract.scala
@@ -1,7 +1,8 @@
 package io.github.sergeiionin.contractsregistrator
 package domain
 
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder, Json}
+import io.circe.generic.semiauto.deriveDecoder
 
 final case class Contract(
                          subject: String,
@@ -11,6 +12,17 @@ final case class Contract(
                          schemaType: SchemaType,
                          isMerged: Boolean = false,
                          deleted: Option[Boolean] = None
-) derives Encoder, Decoder
+) derives Encoder
+
+object Contract:
+  given contractDecoder: Decoder[Contract] = deriveDecoder[Contract].prepare { cursor =>
+    cursor.withFocus { json =>
+      json.hcursor.downField("isMerged").as[Boolean] match {
+        case Left(_) => json.mapObject(_.add("isMerged", Json.fromBoolean(false)))
+        case Right(_) => json
+      }
+    }
+  }
+
 
 final case class SubjectAndVersion(subject: String, version: Int) derives Encoder, Decoder
---
+++ common/src/test/scala/domain/ContractSpec.scala
@@ -0,0 +1,37 @@
+package io.github.sergeiionin.contractsregistrator
+package domain
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import io.circe.syntax.*
+import io.circe.parser.decode
+
+class ContractSpec extends AnyWordSpec with Matchers:
+  "Contract" should {
+    "be encoded" in {
+      val contract = Contract("testSubject", 1, 123, "schema", SchemaType.PROTOBUF, false, None)
+      contract.asJson.noSpaces shouldEqual
+        """{"subject":"testSubject","version":1,"id":123,"schema":"schema","schemaType":"PROTOBUF","isMerged":false,"deleted":null}"""
+    }
+    "be decoded" in {
+      val raw =
+        """
+          |{
+          |  "subject" : "testSubject",
+          |  "version" : 1,
+          |  "id"      : 123, 
+          |  "schemaType" : "PROTOBUF",
+          |  "schema": "syntax = \"proto3\";\npackage schema_registry;\n\nmessage SchemaMessage {\n  string subject = 1;\n  int32 version = 2;\n  int32 id = 3;\n  string schema = 4;\n  bool deleted = 5;\n}\n",
+          |  "deleted": false
+          |}
+          |""".stripMargin
+      val decoded = decode[Contract](raw)
+      decoded.toOption shouldEqual Some(Contract(
+        "testSubject",
+        1,
+        123,
+        "syntax = \"proto3\";\npackage schema_registry;\n\nmessage SchemaMessage {\n  string subject = 1;\n  int32 version = 2;\n  int32 id = 3;\n  string schema = 4;\n  bool deleted = 5;\n}\n",
+        SchemaType.PROTOBUF, false,
+        Some(false)))
+    }
+  }
