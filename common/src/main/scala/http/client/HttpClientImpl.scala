7be27da45b1169a887ee4649b9324084cc2d5f9a
SergeiIonin <sai1042@yandex.ru>
2024-08-22 20:57:01 -0400

    [common] add HttpClient for get/post/delete requests

---
+++ common/src/main/scala/http/client/HttpClientImpl.scala
@@ -0,0 +1,28 @@
+package io.github.sergeiionin.contractsregistrator
+package http.client
+
+import cats.syntax.applicative.*
+import cats.data.EitherT
+import cats.effect.Async
+import org.http4s.{Uri, Response}
+import org.http4s.client.Client
+import org.http4s.EntityEncoder
+import dto.errors.HttpErrorDTO
+
+final class HttpClientImpl[F[_] : Async](client: Client[F]) extends HttpClient[F]:
+  import HttpClient.*
+  def get(uri: Uri, token: Option[String]): EitherT[F, HttpErrorDTO, Response[F]] =
+    client.run(ClientUtils.getRequest(uri, token))
+      .use(resp => resp.pure[F])
+      .toEitherT
+
+  def post[T](uri: Uri, entity: T, token: Option[String])
+                     (using EntityEncoder[F, T]): EitherT[F, HttpErrorDTO, Response[F]] =
+      client.run(ClientUtils.postRequest(uri, token, entity))
+        .use(resp => resp.pure[F])
+        .toEitherT
+
+  def delete(uri: Uri, token: Option[String]): EitherT[F, HttpErrorDTO, Response[F]] =
+      client.run(ClientUtils.deleteRequest(uri, token))
+        .use(resp => resp.pure[F])
+        .toEitherT
