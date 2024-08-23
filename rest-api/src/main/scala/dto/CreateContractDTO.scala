e9c0468b8588be4db4a4ac3094bc9a572a747c6a
SergeiIonin <sai1042@yandex.ru>
2024-08-22 20:58:27 -0400

    [rest-api] fixes to rest-api dto

--- rest-api/src/main/scala/dto/CreateContractDTO.scala
+++ rest-api/src/main/scala/dto/CreateContractDTO.scala
@@ -1,12 +1,17 @@
 package io.github.sergeiionin.contractsregistrator
 package dto
 
-import io.circe.{Decoder, Encoder}
 import domain.SchemaType
 import domain.SchemaType.given
+import dto.schema.SchemaDTO
+
+import io.circe.{Decoder, Encoder}
 import sttp.tapir.Schema
 
 final case class ContractDTO(subject: String, version: Int, schemaType: SchemaType, schema: String) derives Encoder, Decoder, Schema
+object ContractDTO:
+  def fromSchemaDTO(schema: SchemaDTO) = 
+    ContractDTO(schema.subject, schema.version, schema.schemaType, schema.schema)
 final case class CreateContractDTO(schemaType: String = "PROTOBUF",
                                    subject: String,
                                    schema: String) derives Encoder, Decoder, Schema
