8aab3a48266e8dcf2e8cae971f3c19a74f32482c
SergeiIonin <sai1042@yandex.ru>
2024-08-23 20:51:11 -0400

    [rest-api] add CreateContractEndpoints

---
+++ rest-api/src/main/scala/endpoints/CreateContractEndpoints.scala
@@ -0,0 +1,20 @@
+package io.github.sergeiionin.contractsregistrator
+package endpoints
+
+import dto.*
+import sttp.tapir.*
+import sttp.tapir.json.circe.*
+import sttp.tapir.Schema
+import sttp.model.StatusCode
+
+trait CreateContractEndpoints extends BaseContractsEndpoint:
+  
+  val createContract =
+    base.post
+      .in(jsonBody[CreateContractDTO])
+      .out(jsonBody[CreateContractResponseDTO])
+      .name(ContractEndpoint.CreateContract.toString)
+      .description("Create new contract")
+  
+  def getEndpoints: List[AnyEndpoint] =
+    List(createContract)
