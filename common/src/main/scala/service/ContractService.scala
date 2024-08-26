9ef512a277455dfcddfdb3e994c9c994698e4ac7
SergeiIonin <sai1042@yandex.ru>
2024-08-26 14:54:31 -0400

    [common] add ContractServiceImpl

--- common/src/main/scala/service/ContractService.scala
+++ common/src/main/scala/service/ContractService.scala
@@ -1,14 +1,23 @@
 package io.github.sergeiionin.contractsregistrator
 package service
 
-import cats.data.EitherT
-import dto.schema.{Versions, Subjects}
+import domain.Contract
 import dto.ContractDTO
+import domain.{Subjects, Versions}
+import repository.ContractsRepository
+
+import cats.data.EitherT
+import cats.effect.kernel.Resource
 
 trait ContractService[F[_]]:
-  def getContractVersion(subject: String, version: Int): F[Option[ContractDTO]]
-  def getContractVersions(subject: String): F[Versions]
-  def getSubjects(): F[Subjects]
-  def getLatestContract(subject: String): F[Option[ContractDTO]]
+  def saveContract(contract: Contract): F[Unit]
+  def getContractVersion(subject: String, version: Int): F[Option[Contract]]
+  def getContractVersions(subject: String): F[fs2.Stream[F, Int]]
+  def getSubjects(): F[fs2.Stream[F, String]]
+  def getLatestContract(subject: String): F[Option[Contract]]
   def deleteContractVersion(subject: String, version: Int): F[Unit]
   def deleteContract(subject: String): F[Unit]
+
+object ContractService:
+  def make[F[_]](contractRepo: ContractsRepository[F]): Resource[F, ContractService[F]] = 
+    Resource.pure[F, ContractService[F]](ContractServiceImpl[F](contractRepo))
