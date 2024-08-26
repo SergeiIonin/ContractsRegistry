365c626f516f4ec9b21905a79110af82cdb9eea7
SergeiIonin <sai1042@yandex.ru>
2024-08-26 14:57:32 -0400

    [common] add ContractEventKey and ContractEvent to domain.events.contracts

--- common/src/main/scala/domain/events/contracts/ContractEvent.scala
+++ common/src/main/scala/domain/events/contracts/ContractEvent.scala
@@ -1,12 +1,28 @@
 package io.github.sergeiionin.contractsregistrator
 package domain.events.contracts
 
+import domain.Contract
 import domain.events.{Event, Key}
 import io.circe.{Decoder, Encoder}
 
-final case class ContractDeleteRequestedKey(subject: String, version: Option[Int]) extends Key derives Encoder, Decoder
-final case class ContractDeleteRequested(subject: String, version: Option[Int], deleteSubject: Boolean = false) extends Event derives Encoder, Decoder
+sealed trait ContractEventKey extends Key
+sealed trait ContractEvent extends Event
 
-final case class ContractCreateRequestedKey(subject: String, version: Int) extends Key derives Encoder, Decoder
-final case class ContractCreateRequested(subject: String, version: Int) extends Event derives Encoder, Decoder
+final case class ContractDeleteRequestedKey(
+                                             subject: String,
+                                             version: Option[Int]
+                                           ) extends ContractEventKey derives Encoder, Decoder
+final case class ContractDeleteRequested(
+                                          subject: String,
+                                          version: Option[Int],
+                                          deleteSubject: Boolean = false
+                                        ) extends ContractEvent derives Encoder, Decoder
+
+final case class ContractCreateRequestedKey(
+                                             subject: String,
+                                             version: Int
+                                           ) extends ContractEventKey derives Encoder, Decoder
+final case class ContractCreateRequested(
+                                          contract: Contract
+                                        ) extends ContractEvent derives Encoder, Decoder
 
