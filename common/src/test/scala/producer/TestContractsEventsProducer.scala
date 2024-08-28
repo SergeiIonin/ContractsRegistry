af5e16b612bf6d6082257e317a24017061740aa2
SergeiIonin <sai1042@yandex.ru>
2024-08-28 00:27:10 -0400

    [common] refactor producer/TestContractsEventsProducer

--- common/src/test/scala/producer/TestContractsEventsProducer.scala
+++ common/src/test/scala/producer/TestContractsEventsProducer.scala
@@ -4,8 +4,8 @@ package producer
 import cats.Applicative
 import cats.syntax.applicative.*
 import producer.EventsProducer
-import domain.events.contracts.{ContractDeleteRequested, ContractDeleteRequestedKey}
+import domain.events.contracts.{ContractDeletedEventKey, ContractDeletedEvent}
 
-class TestContractsEventsProducer[F[_] : Applicative] extends EventsProducer[F, ContractDeleteRequestedKey, ContractDeleteRequested]:
+class TestContractsEventsProducer[F[_] : Applicative] extends EventsProducer[F, ContractDeletedEventKey, ContractDeletedEvent]:
   override val topic: String = "events_contracts_deleted"
-  override def produce(key: ContractDeleteRequestedKey, value: ContractDeleteRequested): F[Unit] = ().pure[F]
+  override def produce(key: ContractDeletedEventKey, value: ContractDeletedEvent): F[Unit] = ().pure[F]
