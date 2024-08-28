8fb939049b6c3e92a8c20b962f14e8dbbf1a3910
SergeiIonin <sai1042@yandex.ru>
2024-08-28 00:25:23 -0400

    [common] refactor producer/contracts/ContractDeleteEventsKafkaProducer

--- common/src/main/scala/producer/contracts/ContractDeleteEventsKafkaProducer.scala
+++ common/src/main/scala/producer/contracts/ContractDeleteEventsKafkaProducer.scala
@@ -1,7 +1,7 @@
 package io.github.sergeiionin.contractsregistrator
 package producer.contracts
 
-import domain.events.contracts.{ContractDeleteRequested, ContractDeleteRequestedKey}
+import domain.events.contracts.{ContractDeletedEventKey, ContractDeletedEvent}
 import producer.{EventsKafkaProducer, EventsProducer}
 import cats.effect.{Async, Resource}
 import cats.syntax.applicative.*
@@ -9,8 +9,8 @@ import fs2.kafka.{KafkaProducer, ProducerSettings, Serializer}
 
 final class ContractDeleteEventsKafkaProducer[F[_] : Async](
                                                   override val topic: String,
-                                                  override val kafkaProducer: KafkaProducer[F, ContractDeleteRequestedKey, ContractDeleteRequested]
-                                                ) extends EventsKafkaProducer[F, ContractDeleteRequestedKey, ContractDeleteRequested]()
+                                                  override val kafkaProducer: KafkaProducer[F, ContractDeletedEventKey, ContractDeletedEvent]
+                                                ) extends EventsKafkaProducer[F, ContractDeletedEventKey, ContractDeletedEvent]()
 
 object ContractDeleteEventsKafkaProducer:
   import EventsKafkaProducer.producerSettings
@@ -18,7 +18,7 @@ object ContractDeleteEventsKafkaProducer:
   def make[F[_] : Async](
                           contractCreatedTopic: String,
                           bootstrapServers: String,
-                        )(using Serializer[F, ContractDeleteRequestedKey],
-                          Serializer[F, ContractDeleteRequested]): Resource[F, EventsProducer[F, ContractDeleteRequestedKey, ContractDeleteRequested]] =
-    KafkaProducer[F].resource[ContractDeleteRequestedKey, ContractDeleteRequested](producerSettings(bootstrapServers))
+                        )(using Serializer[F, ContractDeletedEventKey],
+                          Serializer[F, ContractDeletedEvent]): Resource[F, EventsProducer[F, ContractDeletedEventKey, ContractDeletedEvent]] =
+    KafkaProducer[F].resource[ContractDeletedEventKey, ContractDeletedEvent](producerSettings(bootstrapServers))
       .map(kafkaProducer => ContractDeleteEventsKafkaProducer[F](contractCreatedTopic, kafkaProducer))
