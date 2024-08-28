1807f052f5da27866fd3921108112d1ff72518ec
SergeiIonin <sai1042@yandex.ru>
2024-08-28 00:48:02 -0400

    [rest-api] rm dto/github/webhooks/PrErrorDTO

--- rest-api/src/main/scala/dto/github/webhooks/PrErrorDTO.scala
+++
@@ -1,9 +0,0 @@
-package io.github.sergeiionin.contractsregistrator
-package dto.github.webhooks
-
-import io.circe.{Decoder, Encoder}
-import sttp.tapir.Schema
-
-sealed trait PrErrorDTO derives Encoder, Decoder, Schema
-
-final case class BadRequestErrorDTO(msg: String) extends PrErrorDTO derives Encoder, Decoder, Schema
