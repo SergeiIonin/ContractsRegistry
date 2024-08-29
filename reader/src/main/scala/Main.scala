61a87fe9d06be71c2253e29e75e6edc0b4f816b0
SergeiIonin <sai1042@yandex.ru>
2024-08-29 00:01:17 -0400

    [reader] rm usage of ContractService from Main

--- reader/src/main/scala/Main.scala
+++ reader/src/main/scala/Main.scala
@@ -8,11 +8,9 @@ import consumers.schemas.{KeyType, SchemasKafkaConsumerImpl}
 import domain.events.contracts.{ContractEvent, ContractEventKey}
 import domain.{Contract, SubjectAndVersion}
 import github.{GitHubClient, GitHubService}
-import handler.ContractsHandler
 import producer.KafkaEventsProducer.given
 import producer.contracts.ContractCreateKafkaEventsProducer
 import repository.ContractsRepository
-import service.ContractService
 
 import cats.data.NonEmptyList
 import cats.effect.{ExitCode, IO, IOApp}
@@ -36,14 +34,13 @@ object Main extends IOApp:
     val kafka = config.kafka
     val postgres = config.postgres
     
-    val topics = List(kafka.schemasTopic, kafka.prsTopic)
     val consumerProps = kafka.consumerProps.toMap()
     
     given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
     given Deserializer[IO, Bytes] = Deserializer.lift(bs => IO.pure(Bytes(bs)))
     
-    val consumerSettings =
+    val schemaConsumerSettings =
       ConsumerSettings
         .apply(
           Deserializer.apply[IO, Bytes],
@@ -62,14 +59,12 @@ object Main extends IOApp:
     (for
       session           <- Session.pooled[IO](host = postgres.host, port = postgres.port, user = postgres.user,
                             database = postgres.database, password = postgres.password.some, max = 10)
-      repo              <- ContractsRepository.make[IO](session)
-      service           <- ContractService.make[IO](repo)
       client            <- EmberClientBuilder.default[IO].build
       contractsProducer <- ContractCreateKafkaEventsProducer.make[IO](kafka.contractsCreatedTopic, kafka.producerProps.bootstrapServers.head)
-      schemasConsumer   <- SchemasKafkaConsumerImpl.make[IO](NonEmptyList.one(kafka.schemasTopic), consumerSettings, service, contractsProducer)
+      schemasConsumer   <- SchemasKafkaConsumerImpl.make[IO](NonEmptyList.one(kafka.schemasTopic), schemaConsumerSettings, contractsProducer)
       gitClient         <- GitHubClient.make[IO](contractConfig.owner, contractConfig.repo, contractConfig.path,
         contractConfig.baseBranch, client, Some(contractConfig.token))
-      gitService        <- GitHubService.make[IO](service, gitClient)
+      gitService        <- GitHubService.make[IO](gitClient)
       contractsConsumer <- ContractsKafkaConsumerImpl.make[IO](
                               NonEmptyList.fromListUnsafe(List(kafka.contractsCreatedTopic, kafka.contractsDeletedTopic)),
                               contractsConsumerSettings, gitService
