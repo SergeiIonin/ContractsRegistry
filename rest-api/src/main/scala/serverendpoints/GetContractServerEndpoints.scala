39353ac8999c1989fc3977d3149769ba02f64f5b
SergeiIonin <sai1042@yandex.ru>
2024-08-29 13:56:25 -0400

    [rest-api] implement GetContractServerEndpoints

--- rest-api/src/main/scala/serverendpoints/GetContractServerEndpoints.scala
+++ rest-api/src/main/scala/serverendpoints/GetContractServerEndpoints.scala
@@ -7,6 +7,7 @@ import dto.schema.*
 import domain.{Versions, Subjects}
 import endpoints.GetContractEndpoints
 import service.ContractService
+import cats.data.EitherT
 import cats.effect.Async
 import cats.syntax.either.*
 import cats.syntax.functor.*
@@ -19,44 +20,28 @@ class GetContractServerEndpoints[F[_] : Async](service: ContractService[F]) exte
     getContractVersion.serverLogic((subject, version) => {
       service
         .getContractVersion(subject, version)
-        .map {
-          case Some(contract) => ContractDTO.fromContract(contract).asRight[HttpErrorDTO]
-          case None => BadRequestDTO(404, s"Contract with subject $subject and version $version not found").asLeft[ContractDTO]
-        }
+        .value
     })
   
   private val getVersionsSE: ServerEndpoint[Any, F] =
     getVersions.serverLogic(subject => {
-      for
-        stream    <- service.getContractVersions(subject)
-        versions  <- stream.compile.toList
-        res       = versions match {
-                      case Nil      => BadRequestDTO(404, s"No versions found for subject $subject").asLeft[List[Int]]
-                      case versions => versions.asRight[HttpErrorDTO]
-                    }
-      yield res
+      service
+        .getContractVersions(subject)
+        .value
     })
   
   private val getSubjectsSE: ServerEndpoint[Any, F] =
     getSubjects.serverLogic(_ => {
-      for
-        stream    <- service.getSubjects()
-        subjects  <- stream.compile.toList
-        res       = subjects match {
-                      case Nil => BadRequestDTO(404, "No subjects found").asLeft[List[String]]
-                      case subjects => subjects.asRight[HttpErrorDTO]
-                    }
-      yield res
+      service
+        .getSubjects()
+        .value
     })
   
   private val getLatestContractSE: ServerEndpoint[Any, F] =
     getLatestContract.serverLogic(subject => {
       service
         .getLatestContract(subject)
-        .map {
-          case Some(contract) => ContractDTO.fromContract(contract).asRight[HttpErrorDTO]
-          case None => BadRequestDTO(404, s"Contract with subject $subject not found").asLeft[ContractDTO]
-        }
+        .value
     })
   
   private val getServerEndpoints: List[ServerEndpoint[Any, F]] =
